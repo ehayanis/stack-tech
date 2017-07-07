3aeafc67d8036b9efd7e6ff889e18cc160918290
ehayanis <hayani.mehdi149@gmail.com>
2017-07-07 14:26:45 +0200

    ajout findAll

--- src/main/java/com/xeigna/ms/demo/controller/PersonController.java
+++ src/main/java/com/xeigna/ms/demo/controller/PersonController.java
@@ -1,9 +1,47 @@
 package com.xeigna.ms.demo.controller;
 
-import org.springframework.stereotype.Controller;
+import com.xeigna.ms.demo.model.Person;
+import com.xeigna.ms.demo.service.PersonService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("api")
 public class PersonController {
 
+    // public static final Logger logger = LoggerFactory.getLogger();
+
+    @Autowired
+    PersonService personService;
+
+    @RequestMapping(value = "/person/", method = RequestMethod.GET)
+    public ResponseEntity<List<Person>> findAll() {
+        List<Person> persons = personService.findAllPersons();
+        if (persons.isEmpty()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<List<Person>>(persons, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/person/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable("id") String id) {
+        Person person = personService.findById(id);
+        if (person == null) {
+            logger.error("User with id {} not found.", id);
+            return new ResponseEntity(new CustomErrorType("User with id " + id
+                    + " not found"), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
 
 }
